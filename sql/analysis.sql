4225a4a3d3b9f6635ac3750af8436e2782b55f4f
Dominik Jur <jur.dominik@gmail.com>
2025-01-19 13:47:32 +0100

    fill

---
+++ sql/analysis.sql
@@ -0,0 +1,7 @@
+SELECT W.id_wycieczki, W.ilosc_uczestnikow, T.kwota,  DATEDIFF(W.data_zakonczenia, W.data_rozpoczecia) as ilosc_dni, K.miasto,K.kraj, T2.nazwa as tematyka
+FROM Wycieczki W
+    JOIN team20.Transakcje T on W.id_wycieczki = T.id_wycieczki
+    JOIN team20.Kierunki K on K.id_kierunku = W.id_kierunku
+    JOIN team20.Tematyki T2 on T2.id_tematyki = W.id_tematyki
+ORDER BY kwota DESC;
+
